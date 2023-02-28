b27f4e942a720c22fde22a8258724aacd62dc0a2
FairLadyy <jegor.sinn33@gmail.com>
2023-02-28 21:28:45 +0200

    com

---
+++ src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { User } from "./users.model";
+import { Role } from "src/roles/roles.model";
+import { UserRoles } from "src/roles/user-roles.module";
+import { RolesModule } from "src/roles/roles.module";
+import { AuthModule } from "src/auth/auth.module";
+import { forwardRef } from "@nestjs/common/utils";
+import { Post } from "src/posts/posts.model";
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles, Post]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
+  exports: [UsersService],
+})
+export class UsersModule {}
